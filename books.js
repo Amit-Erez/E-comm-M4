1039b3b68b60f230f6dd5afd6702c74db6976374
Amit <hazeydays@gmail.com>
2025-07-01 16:41:15 -0700

    E-comm-M4

--- books.js
+++ books.js
@@ -1,28 +1,68 @@
-function renderBooks() {
+
+function filterBooks(event) {
+  renderBooks(event.target.value)
+}
+
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fas fa-star"></i>\n'
+  }
+
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>'
+  }
+  return ratingHTML;
+}
+
+
+function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
-  booksWrapper.innerHTML = `<div class="book">
+  const books = getBooks();
+
+  if (filter === 'LOW_TO_HIGH') {
+    books.sort((a, b) => a.originalPrice - b.originalPrice)
+  }
+
+  else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice)
+  }
+
+  else if (filter === 'RATING') {
+    books.sort((a, b) => b.rating - a.rating)
+  }
+
+  const booksHtml = books.map((book) => {
+    return `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="assets/atomic habits.jpg" alt=""></img>
+      <img class="book__img" src="${book.url}" alt=""></img>
     </figure>
     <div class="book__title">
-      Atomic Habits
+      ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span class="book__price--normal">$59.95</span> $14.95
+      <span>$${book.originalPrice.toFixed(2)}</span>
     </div>
   </div>`;
+  }).join("");
+
+  booksWrapper.innerHTML = booksHtml
 }
+
 
 setTimeout(() => {
   renderBooks();
 });
+
+
+
+
+
+
 
 // FAKE DATA
 function getBooks() {
